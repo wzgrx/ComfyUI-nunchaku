896ec413c747daccb8e417157ed780b9283db766
K <KBRASK@ustc.edu>
2025-05-18 05:38:00 +0800

    feat: PuLID node for nunchaku (#106)
    * add pulid
    
    * The pulid loader and apply functions are separated, allowing for fast inference each time the face is changed.
    
    * style: apply black-jupyter formatting
    
    * style: apply pre-commit formatting

--- nodes/models/__init__.py
+++ nodes/models/__init__.py
@@ -1,4 +1,5 @@
 from .flux import NunchakuFluxDiTLoader
+from .pulid import NunchakuPulidApply, NunchakuPulidLoader
 from .text_encoder import NunchakuTextEncoderLoader
 
-__all__ = ["NunchakuFluxDiTLoader", "NunchakuTextEncoderLoader"]
+__all__ = ["NunchakuFluxDiTLoader", "NunchakuTextEncoderLoader", "NunchakuPulidApply", "NunchakuPulidLoader"]
